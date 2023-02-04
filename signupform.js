49a72f3e6aafe016bcf47b9f1a1595269f6a40df
Shannon Chaulet <shannon.chaulet@gmail.com>
2023-02-04 14:28:52 -0500

    Add better validation for email and names

--- signupform.js
+++ signupform.js
@@ -7,40 +7,137 @@ var email = document.getElementById("user_email");
 var number = document.getElementById("phone_number");
 var error = document.getElementById("error");
 
-// When the user clicks on a field, the border turns blue
+// When the user clicks on a field, or presses any key, the border changes color
 password.onfocus = function() {
     password.style.borderColor = "#e6007e";
 }
 confirmpassword.onfocus = function() {
     confirmpassword.style.borderColor = "#e6007e";
 }
+
 fname.onfocus = function() {
+    if (fname.value.length == 0) {
+        fname.style.borderColor = "#e6007e";
+    }
+    else if (fname.value.length > 0) {
+        fname.style.borderColor = "green";
+    }
+}
+
+fname.onkeydown = function() {
+    if (fname.value.length == 0 && event.key != "Backspace") {
+        fname.style.borderColor = "green";
+    }
+    else if (event.key == "Backspace" && fname.value.length == 1) {
     fname.style.borderColor = "#e6007e";
+    }
+    else if (fname.value.length > 0) {
+        fname.style.borderColor = "green";
+    }
 }
+
 lname.onfocus = function() {
+    if (lname.value.length == 0) {
+        lname.style.borderColor = "#e6007e";
+    }
+    else if (lname.value.length > 0) {
+        lname.style.borderColor = "green";
+    }
+}
+
+lname.onkeydown = function() {
+    if (lname.value.length == 0 && event.key != "Backspace") {
+        lname.style.borderColor = "green";
+    }
+    else if (event.key == "Backspace" && lname.value.length == 1) {
     lname.style.borderColor = "#e6007e";
-}
-email.onfocus = function() {
-    email.style.borderColor = "#e6007e";
-}
-number.onfocus = function() {
-    number.style.borderColor = "#e6007e";
+    }
+    else if (lname.value.length > 0) {
+        lname.style.borderColor = "green";
+    }
 }
 
-// When the user clicks off a field, the border either returns to normal or validates the value
-fname.onblur = function() {
-    fname.style.borderColor = "rgb(133, 133, 133)";
-}
-lname.onblur = function() {
-    lname.style.borderColor = "rgb(133, 133, 133)";
-}
-email.onblur = function() {
-    email.style.borderColor = "rgb(133, 133, 133)";
-}
-number.onblur = function() {
-    number.style.borderColor = "rgb(133, 133, 133)";
+email.onfocus = function() 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(RegForm.email.value))
+  {
+    email.style.borderColor = "green";
+  }
+  else {
+    email.style.borderColor = "red";
+  }
 }
 
+email.onblur = function() 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(RegForm.email.value))
+  {
+    email.style.borderColor = "green";
+  }
+  else {
+    email.style.borderColor = "red";
+  }
+}
+
+email.onkeydown = function() 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(RegForm.email.value))
+  {
+    email.style.borderColor = "green";
+  }
+  else {
+    email.style.borderColor = "red";
+  }
+}
+
+number.onfocus = function() 
+{
+    if (number.value.length < 9)
+    {
+      number.style.borderColor = "red";
+    }
+    else {
+      number.style.borderColor = "green";
+    }
+}
+
+number.onblur = function() 
+{
+    if (number.value.length < 9)
+    {
+      number.style.borderColor = "red";
+    }
+    else {
+      number.style.borderColor = "green";
+    }
+}
+
+number.onkeydown = function() 
+{
+    if (number.value.length < 9)
+    {
+      number.style.borderColor = "red";
+    }
+    else {
+      number.style.borderColor = "green";
+    }
+}
+
+// // When the user clicks off a field, the border either returns to normal or validates the value
+// fname.onblur = function() {
+//     fname.style.borderColor = "rgb(133, 133, 133)";
+// }
+// lname.onblur = function() {
+//     lname.style.borderColor = "rgb(133, 133, 133)";
+// }
+// email.onblur = function() {
+//     email.style.borderColor = "rgb(133, 133, 133)";
+// }
+// number.onblur = function() {
+//     number.style.borderColor = "rgb(133, 133, 133)";
+// }
+
+// Function to check whether or not the password and confirm_password fields are identical
 function CheckPass(password, confirmpassword, error) { 
     if (password.value === confirmpassword.value) {
         password.style.borderColor = "green";
